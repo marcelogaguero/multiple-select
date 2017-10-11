47350fb30a38cf86b365234ad05136a93d018bdf
Marcelo Aguero <marcelo.aguero@nemogroup.net>
2017-10-11 10:19:12 -0300

    script para compilar js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,14 +2,25 @@ var webpack = require('webpack'),
     path = require('path'),
     UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-  entry: path.resolve(__dirname, "../index.js"),
-  devtool: "source-map",
-  output: {
-    path: path.resolve(__dirname,"../dist"),
-    filename: "multi.min.js"
-  },
-  plugins:[
-    new UglifyJSPlugin({})
-  ]
+module.exports = function(env){
+
+  var config = {
+    entry: path.resolve(__dirname, "../index.js"),
+    devtool: "source-map",
+    output: {
+      path: path.resolve(__dirname, "../dist"),
+      filename: "multi.min.js"
+    },
+    plugins: [
+      new UglifyJSPlugin({})
+    ]
+  };
+
+  if ( env && typeof env.prod !== 'undefined' ){
+    config.devtool = "inline-source-map";
+  }
+
+  console.log(config);
+
+  return config;
 };
